ca214a3313a274d7cfc11d706036dc6a297bb440
Vaxxter-sd <carlos213244@gmail.com>
2026-05-22 23:41:22 -0600

    leadfromflask.py

---
+++ crm/models/leadfromflask.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class LeadFromFlask(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    correo = models.EmailField(max_length=100)
+    tipo_escuela = models.CharField(max_length=50)
+    fecha_registro = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        managed = False
+        db_table = 'leads'
+        verbose_name = 'Lead desde Flask'
+        verbose_name_plural = 'Leads desde Flask'
+
+    def __str__(self):
+        return self.nombre
