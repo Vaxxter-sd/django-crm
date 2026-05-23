aacd116c7eefed43581dae6a6439e7da031315c9
Vaxxter-sd <carlos213244@gmail.com>
2026-05-22 22:14:27 -0600

    create_superuser.py

--- create_superuser.py
+++ create_superuser.py
@@ -1,14 +1,10 @@
 import os
 import django
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webcrm.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crm.settings')  # Cambia 'crm' por tu nombre de proyecto
 django.setup()
 
 from django.contrib.auth import get_user_model
 User = get_user_model()
-
 if not User.objects.filter(username='admin').exists():
     User.objects.create_superuser('jose', 'carlos213244@gmail.com', 'carlos2306')
-    print("Superusuario creado")
-else:
-    print("Superusuario ya existe")
